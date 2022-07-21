1cb5285204b86616dc712fdfd7d6a89d97ff841a
tkl <tluk92@gmail.com>
2022-07-20 22:44:28 -0400

    wip: created basic framework for page model. currently no export

---
+++ models/page.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+/*
+///// Basic Framework for Page /////
+
+EventName: Event name/identifier. keep as a string for reference purposes || (String)
+EventTimer: Time taken for event. Forces climax/conclusion when certain amount is reached || (Number)
+ownership: userEmail OR Unique identifier. Used for permission to modify || (String)
+createdBy: userName, links with next line || (String)
+private: createdBy = userName OR Anonymous || (Boolean)
+
+*/
+
+const newPage = new Schema ({
+    eventName: { type: String, required: true},
+    eventTimer: { type: Number, required: true},
+    belongsTo: { type: String, required: true},
+    createdBy: { type: String, required: true},
+    private: { type: Boolean, default: false} 
+}, {timestamps})

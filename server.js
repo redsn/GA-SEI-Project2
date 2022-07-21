075fb7fb23f0acf8259728d370370f2039a355d3
tkl <tluk92@gmail.com>
2022-07-20 23:15:25 -0400

    wip: mounted userRouter

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ require('dotenv').config();
 const express = require('express');
 const methodOverride = require('method-override');
 const mongoose = require('mongoose');
+const pageRouter = require('./controllers/pages')
+const userRouter = require('./controllers/users')
 
 // Usage
 const app = express();
@@ -12,13 +14,19 @@ app.use(express.urlencoded({extended: false}));
 const PORT = 4000; /// Change with .env var later
 //////// mongodb connections here later /////////
 
+// MAIN PAGE // *** WIP
+app.get('/', (req,res) => {
+    res.send('Here I am, once again. I\'m torn into pieces')
+})
+///////////////
 
 
 
 
 
-
-
+//////// Mount Routers ///////
+app.use('/user', userRouter) // 'user' route is temp. TBD 
+// app.use('/user', pageRouter) // Placeholder, not yet implemented
 
 /// Listener ///
 app.listen(PORT, console.log(`Application loaded on port: ${PORT}`));

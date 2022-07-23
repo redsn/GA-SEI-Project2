749ac8ebeee41c0744dca91ff3e945fbb1c0d318
tkl <tluk92@gmail.com>
2022-07-23 19:32:15 -0400

    wip: starting pages via routes

--- controllers/pages.js
+++ controllers/pages.js
@@ -8,5 +8,19 @@ const pageRouter = express.Router();
 const Page = require('../models/page');
 const User = require('..//models/user');
 
+//INDEX//
+
+//NEW//
+
+//DELETE//
+
+//UPDATE//
+
+//CREATE//
+
+//EDIT//
+
+//SHOW//
+
 ///// EXPORTS /////
 module.exports = pageRouter;

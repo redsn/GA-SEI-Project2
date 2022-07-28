5f0007353f17c9f4815f6b9bd4bb5d1a33bab8d9
tkl <tluk92@gmail.com>
2022-07-28 00:16:47 -0400

    wip: updated book model to include pagesMax, private w/ default

--- models/book.js
+++ models/book.js
@@ -3,8 +3,10 @@ const Schema = mongoose.Schema;
 
 const newBook = new Schema ({
     title: { type: String },
+    pagesMax: { type: Number },
     pages: { type: Array },
     choices: { type: Array },
+    private: { type: Boolean, default: true},
     results: { type: Array }
 })
 

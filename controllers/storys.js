dd716cb50ec0d3dc456a34b5327981a68f0de2fa
tkl <tluk92@gmail.com>
2022-07-30 12:43:06 -0400

    wip: building out story route

---
+++ controllers/storys.js
@@ -0,0 +1,25 @@
+// Dependencies //
+const express = require('express');
+
+// Router //
+const storyRouter = express.Router();
+
+// Models //
+const Page = require('../models/page');
+const User = require('../models/user');
+const Book = require('../models/book');
+const Story = require('../models/story');
+
+// INDEX //
+
+// NEW //
+
+// DELETE //
+
+// UPDATE //
+
+// CREATE //
+
+// EDIT //
+
+// SHOW //
---
+++ models/story.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const newStory = new Schema({
+    title: { type: String },
+    pagesMax: { type: Number },
+    pages: { type: Array },
+    choiceA: { type: Array },
+    choiceAText: { type: Array },
+    choiceB: { type: Array },
+    choiceBText: { type: Array },
+    private: { type: Boolean, default: true},
+    belongsTo: { type: String },
+    complete: { type: Boolean, default: false}, 
+    results: { type: Array }
+})
+
+const Story = mongoose.model('Story', newStory);
+
+module.exports = Story;
+
+
+/// Same as book at start, to be reviewed later

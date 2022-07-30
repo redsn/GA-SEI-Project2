79b2dda7c6b0d6786996891156233d7303ba546e
tkl <tluk92@gmail.com>
2022-07-30 14:43:48 -0400

    wip: build frame for views/story/edit, now to build put route

--- controllers/storys.js
+++ controllers/storys.js
@@ -12,8 +12,10 @@ const Story = require('../models/story');
 
 // INDEX //
 
+
 // NEW //
 storyRouter.post('/new', (req,res) => {
+    console.log(req.body)
     Story.create(req.body, (err, newStory) => {
         if(err){
             res.redirect('/');
@@ -27,9 +29,34 @@ storyRouter.post('/new', (req,res) => {
 
 // UPDATE //
 
+
 // CREATE //
 
 // EDIT //
+//Story creation "edit"//
+storyRouter.get('/final/:storyID/:page', (req,res) => {
+    if(!req.session.user){
+        res.redirect('/')
+    } else {
+Story.findById(req.params.storyID, (err, findStory)=>{
+    Book.findById(findStory.bookID, (err, indexBook) => {
+
+        console.log(`indexBook: ${indexBook}`)
+
+        Page.findById(indexBook.pages[req.params.page], (err, currentPage) => {
+
+                res.render('./storys/edit.ejs', {
+                    book: indexBook,
+                    page: currentPage,
+                    story: findStory,
+                    STORYID: req.params.storyID,
+                    PAGEID: req.params.page
+                })
+        })
+    })
+})
+}
+})
 
 // SHOW //
 
